eadbfcc5977fc874afd028554970aa2479a57043
DrMohamedElsherif <dr_mohamed_elsherif@yahoo.com>
2024-06-20 19:04:18 +0200

    Initial commit

--- DBSonlyPatients.R
+++ DBSonlyPatients.R
@@ -6,15 +6,19 @@ library(dplyr)
 LEDD_Concomitant_Medication_Log_Calculated <- read_csv("LEDD_Concomitant_Medication_Log_Calculated.csv")
 Surgery_for_PD_Log <- read_csv("Surgery_for_PD_Log_20Jun2024.csv")
 
+# Filter the Surgery_for_PD_Log table to contain only patients with value 2 in the PDSRGLOC column
+Surgery_for_PD_Log <- Surgery_for_PD_Log %>%
+  filter(PDSRGLOC == 2)
+
 # Find the patient numbers in LEDD_Concomitant_Medication_Log_Calculated that are also present in Surgery_for_PD_Log
 DBS_patients <- LEDD_Concomitant_Medication_Log_Calculated %>%
   filter(PATNO %in% Surgery_for_PD_Log$PATNO)
 
 # Save the extracted data to a new CSV file
-write.csv(common_patients, file = "LEDD_Concomitant_Medication_Log_Calculated_DBSonly.csv", row.names = FALSE)
+write.csv(DBS_patients, file = "LEDD_Concomitant_Medication_Log_Calculated_STN_DBSonly.csv", row.names = FALSE)
 
 # Count the number of unique patient numbers in the new table
-unique_patient_count <- n_distinct(common_patients$PATNO)
+unique_patient_count <- n_distinct(DBS_patients$PATNO)
 
 # Print the number of unique patient numbers
 print(paste("Number of unique patient numbers:", unique_patient_count))
